9d699fe540b5df06088217b031243b00b3bdd51b
mhsun1992 <mhsun1992@gmail.com>
2015-10-16 15:46:28 +0800

    add hooks

---
+++ hooks/pre-commit.php
@@ -0,0 +1,27 @@
+#!/usr/bin/php
+<?php
+
+$exit_status = 0;
+
+exec("pwd", $path_list);
+
+exec("phpalizer run {$path_list[0]}", $output_list);
+
+while (($output = array_shift($output_list)) && substr($output, 0, 1) !='-');
+
+foreach ($output_list as $output)
+{
+    if ($output != '' && $output != 'Done')
+    {
+        $exit_status = 1;
+        echo $output, "\n";
+    }
+}
+
+if ($exit_status)
+{
+    echo "\n";
+}
+
+exit($exit_status);
+
